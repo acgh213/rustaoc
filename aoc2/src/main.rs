fc82365c8ec41731cd20d3f4eac006385ee39087
Andie Eido <406596+acgh213@users.noreply.github.com>
2022-12-02 20:27:03 -0500

    day 2

--- aoc2/src/main.rs
+++ aoc2/src/main.rs
@@ -1,35 +1,77 @@
-use std::char;
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{env, io};
 
-fn process_lines(reader: BufReader<File>) {
-    for line in reader.lines() {
-        let line = line.unwrap(); // unwrap the Result to get the line
-
-        // compare the first and last characters of the line
-        let first_char = line.chars().next().unwrap();
-        let last_char = line.chars().rev().next().unwrap();
-        let mut total = 0;
-        if first_char == A {
-           match last_char {
-             X => total = total + 
-             Y =>
-             Z =Z
-           }
-        } else {
-            println!("The first and last characters are not equal");
-        }
-
-        // perform arithmetic on the first and last characters
-        let n1 = first_char; // convert to u32
-        let n2 = last_char; // convert to u32
-
-        println!("Outputs: {} {})",n1,n2);
+fn part1(lines: &Vec<&str>) {
+    let mut answer: u64 = 0;
+    for line in lines {
+        let p1 = line.chars().nth(0).unwrap();
+        let p2 = line.chars().nth(2).unwrap();
+        answer += match p1 {
+            'A' => match p2 {
+                'X' => 4,
+                'Y' => 8,
+                'Z' => 3,
+                _ => panic!(),
+            },
+            'B' => match p2 {
+                'X' => 1,
+                'Y' => 5,
+                'Z' => 9,
+                _ => panic!(),
+            },
+            'C' => match p2 {
+                'X' => 7,
+                'Y' => 2,
+                'Z' => 6,
+                _ => panic!(),
+            },
+            _ => panic!(),
+        };
     }
+
+    println!("PART 1: {answer}");
+}
+
+fn part2(lines: &Vec<&str>) {
+    let mut answer: u64 = 0;
+    for line in lines {
+        let p1 = line.chars().nth(0).unwrap();
+        let p2 = line.chars().nth(2).unwrap();
+        answer += match p1 {
+            'A' => match p2 {
+                'X' => 3,
+                'Y' => 4,
+                'Z' => 8,
+                _ => panic!(),
+            },
+            'B' => match p2 {
+                'X' => 1,
+                'Y' => 5,
+                'Z' => 9,
+                _ => panic!(),
+            },
+            'C' => match p2 {
+                'X' => 2,
+                'Y' => 6,
+                'Z' => 7,
+                _ => panic!(),
+            },
+            _ => panic!(),
+        };
+    }
+
+    println!("PART 1: {answer}");
 }
 
 fn main() {
-    let file = File::open("./input/2.txt").unwrap();
-    let reader = BufReader::new(file);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Invalid Usage. Usage: ./[binary] [input]");
+    }
 
-    process_lines(reader);
+    let read_result: io::Result<String> = std::fs::read_to_string(&args[1]);
+    let Ok(unwrapped): Result<String, io::Error> = read_result else { panic!("Error reading {}", args[1]) };
+    let lines: Vec<&str> = FromIterator::from_iter(unwrapped.split('\n'));
+
+    part1(&lines);
+    part2(&lines);
 }
